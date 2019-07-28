a2b6060c1cc2c40ada800a4dedae384e0e366b35
Albert Gabdullin <albert.gabdullin@piano.io>
2019-07-28 04:18:14 +0300

    Updates

---
+++ precache-manifest.ae0d5e080db1965666de1a00d939d260.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d63d02fd9679b91149b3260bd2bdd67a",
+    "url": "albertgabdullin.github.io/puzzle15/static/media/numberBackground.d63d02fd.svg"
+  },
+  {
+    "revision": "9203b416b5aa697c72080ee28a119490",
+    "url": "albertgabdullin.github.io/puzzle15/static/media/button.9203b416.svg"
+  },
+  {
+    "revision": "0df525694831df21de4ab857a8aecd53",
+    "url": "albertgabdullin.github.io/puzzle15/static/media/board.0df52569.svg"
+  },
+  {
+    "revision": "82b4b2995a81750068b08120ee3f9fa7",
+    "url": "albertgabdullin.github.io/puzzle15/static/media/background.82b4b299.svg"
+  },
+  {
+    "revision": "e9f98e015278484ef4b7",
+    "url": "albertgabdullin.github.io/puzzle15/static/js/runtime~main.fea8dbbb.js"
+  },
+  {
+    "revision": "3dac17dc20454ad26f1b",
+    "url": "albertgabdullin.github.io/puzzle15/static/js/main.240469a7.chunk.js"
+  },
+  {
+    "revision": "0b2c2c996fde90c78232",
+    "url": "albertgabdullin.github.io/puzzle15/static/js/2.ec565d28.chunk.js"
+  },
+  {
+    "revision": "3dac17dc20454ad26f1b",
+    "url": "albertgabdullin.github.io/puzzle15/static/css/main.8bd3222a.chunk.css"
+  },
+  {
+    "revision": "2995818e1a5ca3b55acabcbfbce40697",
+    "url": "albertgabdullin.github.io/puzzle15/index.html"
+  }
+];
