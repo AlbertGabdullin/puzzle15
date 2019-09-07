6e3556421507eabe85c29432c646e73bb12d608b
Albert Gabdullin <albert.gabdullin@piano.io>
2019-09-07 22:22:25 +0300

    remove header from board

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Board from './containers/Board';
-import Header from './containers/Header';
 import './App.css';
 
 
-const App = props => (
-  <Fragment>
-    <Header />
-    <Board {...props} />
-  </Fragment>
-)
+const App = props => <Board {...props} />;
 
 const mapStateToProps = state => ({
   numbers: state.game.matrix,
