f279762588995004c422309177fcc4d2beb548c3
Albert Gabdullin <albert.gabdullin@piano.io>
2020-07-07 23:22:41 +0300

    add memo to matrix component

--- src/components/Matrix/index.jsx
+++ src/components/Matrix/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useMemo, memo } from 'react';
 import memoize from 'memoize-one';
 import Tile from '../Tile';
 import type { GameMatrix } from '../../types';
@@ -19,37 +19,43 @@ type Props = {
   move: (line: number, column: number) => void,
 };
 
-const Matrix = ({ matrix, width, height, boardSize, move }: Props) => {
-  const newArray = [];
-  matrix.forEach((item, line) => {
-    item.forEach((child, column) => {
-      const el = {
-        number: child,
-        line,
-        column,
-      };
-      newArray.push(el);
+const Matrix = memo<Props>(
+  ({ matrix, width, height, boardSize, move }: Props) => {
+    const matrixArr = useMemo(() => {
+      const newArray = [];
+      matrix.forEach((item, line) => {
+        item.forEach((child, column) => {
+          const el = {
+            number: child,
+            line,
+            column,
+          };
+          newArray.push(el);
+        });
+      });
+
+      return newArray;
+    }, [matrix]);
+
+    // $FlowFixMe
+    const tiles = matrixArr.map((item, index) => {
+      const style = getStyles(boardSize, width, height, index);
+
+      return (
+        <Tile
+          x={style.x}
+          y={style.y}
+          number={item.number}
+          line={item.line}
+          column={item.column}
+          move={move}
+          key={item.number}
+        />
+      );
     });
-  });
 
-  // $FlowFixMe
-  const tiles = newArray.map((item, index) => {
-    const style = getStyles(boardSize, width, height, index);
-
-    return (
-      <Tile
-        x={style.x}
-        y={style.y}
-        number={item.number}
-        line={item.line}
-        column={item.column}
-        move={move}
-        key={item.number}
-      />
-    );
-  });
-
-  return tiles;
-};
+    return tiles;
+  },
+);
 
 export default Matrix;
